12519eacda9963f840809eabf1f5bcd40ee8504a
zuckung <zuckung@gmx.de>
2025-12-18 22:52:45 +0100

    fix unpacking

--- res/src/check_updates.py
+++ res/src/check_updates.py
@@ -69,57 +69,6 @@ def unpack(version, vpath, lastmodifiedO):
 	# modifying index.html
 	print('  unpacking zip')
 	archive = zipfile.ZipFile(vpath + version + '.zip')
-	# if release
-	if vpath == 'tmp/release/':
-		with open('page/index.html', 'r') as source: # writing version to page/index.html
-			lines = source.readlines()
-		with open('page/index.html', 'w') as target:
-			for line in lines:
-				if line.find('release/index') > 0:
-					line = '<td style="width:200px"><a href="release/index.html" style="color: #80808;">release (' + version + ')</a></td>\n'
-				elif line.find('release update') > 0:
-					line = '<td style="font-size:11px;">release update: <br>[' + str(lastmodifiedO).split(' ')[0] + ']</td>\n'
-				target.write(line)
-	# if android
-	#if vpath == 'tmp/android/':
-	#	with open('page/index.html', 'r') as source: # writing version to page/index.html
-	#		lines = source.readlines()
-	#	with open('page/index.html', 'w') as target:
-	#		for line in lines:
-	#			if line.find('android/index') > 0:
-	#				line = '<td style="width:200px"><a href="android/index.html" style="color: #80808;">android (' + version + ')</a></td>\n'
-	#			elif line.find('android update') > 0:
-	#				line = '<td style="font-size:11px;">android update: <br>[' + str(lastmodifiedO).split(' ')[0] + ']</td>\n'
-	#			target.write(line)
-	# if continuous
-	#if vpath == 'tmp/continuous/':
-	#	with open('page/index.html', 'r') as source: # writing version to page/index.html
-	#		lines = source.readlines()
-	#	with open('page/index.html', 'w') as target:
-	#		for line in lines:
-	#			if line.find('continuous/index') > 0:
-	#				line = '<td style="width:200px"><a href="continuous/index.html" style="color: #808080;">continuous (' + version + ' +)</a></td>\n'
-	#			elif line.find('continuous update') > 0:
-	#				line = '<td style="font-size:11px;">continuous update: <br>[' + str(lastmodifiedO).split(' ')[0] + ']</td>\n'
-	#			target.write(line)		
-	# unpacking
-	for file in archive.namelist():
-		# if release
-		if vpath == 'tmp/release/':
-			if file.startswith('data/') or file.startswith('images/'):
-				archive.extract(file, vpath)
-		# if android				
-		#elif vpath == 'tmp/android/':
-		#	if file.startswith('endless-mobile-android/data/') or file.startswith('endless-mobile-android/images/'):
-		#		archive.extract(file, vpath)
-		#		shutil.copytree('tmp/android/endless-mobile-android/', 'tmp/android/', dirs_exist_ok=True)
-		#		shutil.rmtree('tmp/android/endless-mobile-android/')
-		# if continuous
-		#elif vpath == 'tmp/continuous/':
-		#	if file.startswith('endless-sky-master/data/') or file.startswith('endless-sky-master/images/'):
-		#		archive.extract(file, vpath)
-		#		shutil.copytree('tmp/continuous/endless-sky-master/', 'tmp/continuous/', dirs_exist_ok=True)
-		#		shutil.rmtree('tmp/continuous/endless-sky-master/')
 	os.remove(vpath + version + '.zip')
 	print('    unpacking done')
 
